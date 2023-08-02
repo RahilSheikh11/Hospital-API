bf574e825740211acf769cd5fda595b041622118
RahilSheikh11 <138210938+RahilSheikh11@users.noreply.github.com>
2023-08-02 12:14:28 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,123 @@
+
+//DoctorID's made till now to use:
+    
+// _id
+// 64b77fc521b50b017d3fa40b
+// name
+// "rahil"
+// password
+// "123456"
+// __v
+// 0
+// _id
+// 64b7ec0dbb2d11db68b2f677
+// name
+// "Rahul"
+// password
+// "123456"
+// __v
+// 0
+// _id
+// 64b7f527960668fb0aece996
+// name
+// "ajay"
+// password
+// "123456"
+// __v
+// 0
+// _id
+// 64b7f85e1d14f818da8c95ed
+// name
+// "coding"
+// password
+// "123456"
+// __v
+// 0
+
+// Patients made till now:
+
+// _id
+// 64b7881d1ac7980669b3b19c
+// name
+// "Rahilahemad"
+// doctor
+// 64b77fc521b50b017d3fa40b
+
+// reports
+// Array
+// __v
+// 2
+// _id
+// 64b7ed155ac56cde9558972b
+// name
+// "Rahulahemad"
+// doctor
+// 64b7ec0dbb2d11db68b2f677
+
+// reports
+// Array
+// __v
+// 1
+// _id
+// 64b7f560960668fb0aece998
+// name
+// "Abdul"
+// doctor
+// 64b7ec0dbb2d11db68b2f677
+
+// reports
+// Array
+// __v
+// 1
+// _id
+// 64b7f60b960668fb0aece9a5
+// name
+// "Coding ninjas"
+// doctor
+// 64b7ec0dbb2d11db68b2f677
+
+// reports
+// Array
+// __v
+// 0
+// _id
+// 64b7f8711d14f818da8c95ef
+// name
+// "ninjas"
+// doctor
+// 64b7ec0dbb2d11db68b2f677
+
+// reports
+// Array
+// __v
+// 1
+// _id
+// 64be35f69bfd48446dfe676c
+// name
+// "ninjas123"
+// doctor
+// 64b7ec0dbb2d11db68b2f677
+
+// reports
+// Array
+// __v
+// 1
+
+//Please mention the problem correctly TA you are confusing be elaborate so we both can finish our work brother.
+//I truly did not understand this TA says:
+//please provide a way so that we can get the id from response so that we can work on further api
+
+
+
+
+
+
+
+
+
+
+
+
 const Doctor = require ('../models/doctor');
 const Patient = require ('../models/Patient');
 
